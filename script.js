ca7d71d50f9e44e068472b623538d9e928b7febf
kaekld <kledmonroy@gmail.com>
2025-09-04 10:34:19 -0600

    C.P y Sexo

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const age = document.getElementById('age');
     const name = document.getElementById('name');
     const cp = document.getElementById('cp');
+    const sex = document.getElementById('sex');
 
     // evento para el input en el campo del curp 
     curp.addEventListener('input', () => {
@@ -29,13 +30,31 @@ document.addEventListener('DOMContentLoaded', () => {
         if (age_copy > 120) age_copy = age_copy.slice(0, 2);
         age_copy = age_copy.slice(0,4).replace(/[^0-9]/g,'')
 
-        age.value = age_copy
+        age.value = age_copy;
 
     })
 
-    name.addEventListener('input', () =>{
-        let name_copy = name.value;
-        // Pendiente...
+    // eventos relacionados con el codigo postal
+    cp.addEventListener('input', () => {
+        let cp_copy = cp.value;
+        cp_copy = cp_copy.slice(0,5).replace(/[^0-9]/g,'')
+        cp.value = cp_copy
+    })
+    cp.addEventListener('blur', () => {
+        let cp_copy = cp.value;
+        while (cp_copy.length < 5){
+            cp_copy = cp_copy.slice(0, cp_copy.length) + '0'
+        }
+        cp.value = cp_copy
+    })
+    cp.addEventListener('focus', () => {
+        cp.select();
+    })
+
+    sex.addEventListener('input', () => {
+        let sex_copy = sex.value;
+        sex_copy = sex_copy.slice(0,1).replace(/[^HM]/g,'')
+        sex.value = sex_copy
     })
 
 
